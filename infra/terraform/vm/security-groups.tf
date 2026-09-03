8e921cef3c592c66ceb13c4947cd9a894deb7f8c
kauan-matheus <kauan8643@gmail.com>
2026-09-03 16:58:23 -0300

    [feat][infra]: liberando portas na AWS para monitoramento

--- infra/terraform/vm/security-groups.tf
+++ infra/terraform/vm/security-groups.tf
@@ -32,6 +32,7 @@ resource "aws_security_group" "ec2_workstation" {
 }
 
 resource "aws_security_group" "ec2_homolog" {
+
   name        = "ec2-homolog-sg"
   description = "Security group for the homolog EC2"
   vpc_id      = aws_vpc.workstation_vpc.id
@@ -40,6 +41,30 @@ resource "aws_security_group" "ec2_homolog" {
     description = "SSH from allowed CIDR"
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_allowed_cidr]
+  }
+
+  ingress {
+    description = "Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_allowed_cidr]
+  }
+
+  ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_allowed_cidr]
+  }
+
+  ingress {
+    description = "Portainer"
+    from_port   = 9443
+    to_port     = 9443
     protocol    = "tcp"
     cidr_blocks = [var.ssh_allowed_cidr]
   }
@@ -57,14 +82,40 @@ resource "aws_security_group" "ec2_homolog" {
 }
 
 resource "aws_security_group" "ec2_prod" {
+
   name        = "ec2-prod-sg"
   description = "Security group for the prod EC2"
-  vpc_id      = aws_vpc.workstation_vpc.id
+
+  vpc_id = aws_vpc.workstation_vpc.id
 
   ingress {
     description = "SSH from allowed CIDR"
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_allowed_cidr]
+  }
+
+  ingress {
+    description = "Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_allowed_cidr]
+  }
+
+  ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_allowed_cidr]
+  }
+
+  ingress {
+    description = "Portainer"
+    from_port   = 9443
+    to_port     = 9443
     protocol    = "tcp"
     cidr_blocks = [var.ssh_allowed_cidr]
   }
